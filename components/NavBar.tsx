491dbff9af413669aaa17cd486328eb585bd2886
Dennis Chen <proofprogram@gmail.com>
2021-08-12 15:40:39 -0700

    some navvbar items

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,17 @@
 import { FC } from "react";
+import NextLink from "next/link";
+
+const NavBarItem: FC<{}> = ({children}) => (
+    <NextLink href = {`${children}`}><a className="capitalize">{children}</a></NextLink>
+);
 
 const NavBar: FC<{}> = () => {
     return (
-        <>
-            
-        </>
+        <div className = "space-x-6 flex text-xl">
+            <NavBarItem>Math</NavBarItem>
+            <NavBarItem>Writing</NavBarItem>
+            <NavBarItem>Code</NavBarItem>
+        </div>
     );
 };
 
