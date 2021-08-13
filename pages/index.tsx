cfd60ea3c7d509fbcf1865dbc211d07afe68abe5
Dennis Chen <proofprogram@gmail.com>
2021-08-12 19:53:49 -0700

    link

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,26 +5,26 @@ const Item: FC<{
   name: string;
   link: string;
 }> = ({ children, name, link }) => (
-  <span><Link href = {`${link}`}><a>{name}</a></Link>&nbsp;{children}</span>
+  <span><Link href = {`${link}`}><a>{name}</a></Link><p className = "pl-6">{children}</p></span>
 );
 
 const Home: FC<{}> = () => {
   return (
-    <div className = "mx-5">
+    <div className = "mx-20 space-y-2">
       <h1 className = "text-5xl font-bold">Dennis Chen's Website</h1>
       <p>Welcome to my personal website. You can find my math exposition, writing, and code here.</p>
       <h2 className = "text-3xl font-bold">Top Links</h2>
       <p>These are the projects that people tend to be the most interested in.</p>
       <h3 className = "text-xl font-bold">Math</h3>
-      <ul>
+      <ul className = "-ml-6">
         <li>
           <Item name = "Careful!" link = "resources/careful.pdf">A handout with commonly missed problems on select math contests and some psychology on how to avoid mistakes.</Item>
         </li>
       </ul>
       <h3 className = "text-xl font-bold">Writing</h3>
-      <ul>
+      <ul className = "-ml-6">
         <li>
-          <Item name = "Contest Report" link = "reports/contest-creation.pdf">Written with William Dai. This is a reflection on the contests I've ran under MAC and some advice for those interested in running their own contests.</Item> (The TeX source is available <Link href = "reports/contest-creation.tex"><a>here</a></Link>.)
+          <Item name = "Contest Report" link = "reports/contest-creation.pdf">Written with William Dai. This is a reflection on the contests I've ran under MAC and some advice for those interested in running their own contests.(The TeX source is available <Link href = "reports/contest-creation.tex"><a>here</a></Link>.)</Item>
         </li>
       </ul>
       <h3 className = "text-xl font-bold">Code</h3>
