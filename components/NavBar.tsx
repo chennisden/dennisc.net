21befba4d39833de20d96865d4d69cc1a5ba4733
Dennis Chen <dchen@dennisc.net>
2021-08-24 20:39:18 -0700

    ok

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,27 +11,31 @@ const NavBarItem: FC<{}> = ({ children }) => (
 
 const NavBar: FC<{}> = () => {
     return (
-        <div className="bg-black px-28 flex text-xl items-center">
-            <NextLink href="/">
-                <a>
-                    <img
-                        style={{ height: "35px" }}
-                        src="dcwhite.svg"
-                        className="text-white"
-                        alt="dc"
+        <div className="bg-black">
+            <div className="self-justify-center max-w-screen-md">
+                <div className="mx-12 sm:mx-28 flex text-xl justify-between items-center">
+                    <NextLink href="/">
+                        <a>
+                            <img
+                                style={{ height: "35px" }}
+                                src="dcwhite.svg"
+                                className="text-white"
+                                alt="dc"
+                            />
+                        </a>
+                    </NextLink>
+                    <div
+                        style={{
+                            flexGrow: 1,
+                            flexDirection: "row",
+                            justifyContent: "flex-end",
+                        }}
                     />
-                </a>
-            </NextLink>
-            <div
-                style={{
-                    flexGrow: 1,
-                    flexDirection: "row",
-                    justifyContent: "flex-end",
-                }}
-            />
-            <NavBarItem>math</NavBarItem>
-            <NavBarItem>writing</NavBarItem>
-            <NavBarItem>code</NavBarItem>
+                    <NavBarItem>math</NavBarItem>
+                    <NavBarItem>writing</NavBarItem>
+                    <NavBarItem>code</NavBarItem>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ pages/writing.tsx
@@ -0,0 +1,12 @@
+import Link from '../components/Link';
+import { FC } from "react";
+
+const Writing: FC<{}> = () => {
+	return (
+		<div>
+
+		</div>
+	);
+}
+
+export default Writing;
