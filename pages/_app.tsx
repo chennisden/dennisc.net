9befa867be2ca8a3ffa6e2c997ed787458f7854b
Dennis Chen <proofprogram@gmail.com>
2021-08-25 23:00:13 -0700

    correctly render based on router.pathname

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,23 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import "../styles/global.css";
+import { useRouter } from "next/router";
+import "styles/global.css";
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import Back from "components/Back";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <div>
       <Head>
         <title>Dennis Chen</title>
         <link rel="shortcut icon" type="image/x-icon" href="dc.svg" />
       </Head>
-
       <div className="min-h-screen flex flex-col">
         <NavBar />
         <div className="my-6" />
-        <Back href="." />
+        {router.pathname != "/" && <Back href="." />}
         <Component {...pageProps} />
         <div className="flex-grow my-6" />
         <Footer />
