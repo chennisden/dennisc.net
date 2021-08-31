9276d88f1afe8c73500d7a4cc3f83418e9cfbe70
Dennis Chen <proofprogram@gmail.com>
2021-08-30 21:01:48 -0700

    Wow, it works!

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -4,7 +4,7 @@ import fs from "fs";
 import matter from "gray-matter";
 import { bundleMDX } from "mdx-bundler";
 import path from "path";
-import {getPathsRecurse } from "@/utils/PageUtils";
+import { getPathsRecurse } from "@/utils/PageUtils";
 import { GetStaticProps, GetStaticPaths } from "next";
 
 export default function PostPage(props) {
@@ -20,9 +20,17 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     slug = params.slug;
   }
 
-  const pagePath = `pages${slug.map((str) => `/${str}`)}.mdx`;
+  var pagePath;
+  if (slug == undefined) {
+    pagePath = "pages/index";
+  } else {
+    pagePath = `pages${slug.map((str) => `/${str}`).join("")}`;
+    if (fs.existsSync(pagePath) && fs.statSync(pagePath).isDirectory()) {
+      pagePath += "/index";
+    }
+  }
+  pagePath += ".mdx";
 
-  console.log(pagePath);
   const source = fs.readFileSync(pagePath);
   const { content, data } = matter(source);
 
@@ -58,17 +66,19 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-
   const allFiles = getPathsRecurse("pages", []).map((file) => {
     const fileAsArray = file.split("/");
-    fileAsArray.splice(0, 1);
     return fileAsArray;
   });
 
   const paths = allFiles
     .filter((path) => /\.mdx$/.test(path[path.length - 1]))
     .map((path) => {
+      path.splice(0, 1);
       path[path.length - 1] = path[path.length - 1].replace(/\.mdx$/, "");
+      if (path[path.length - 1] === "index") {
+        path.pop();
+      }
       return {
         params: {
           slug: path,
